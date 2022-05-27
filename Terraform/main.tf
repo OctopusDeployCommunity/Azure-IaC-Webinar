fed097d4dfc709ba47339891af4e551cf1befbf0
Derek Campbell <Derek.Campbell@octopus.com>
2022-05-27 11:11:39 +0100

    Adding Terraform Template

---
+++ Terraform/main.tf
@@ -0,0 +1,46 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
+}
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup-${random_integer.ri.result}"
+  location = "eastus"
+}
+# Create the Linux App Service Plan
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  source_control {
+    repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+}
